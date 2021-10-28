b8a93ebab11739e4863458f782c962482da19c68
tatianadavila <tatianadavila@elecsis.com>
2021-10-28 18:35:41 -0500

    Integrate base_calculator branch with advanced_calculator branch

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,4 +6,13 @@ public class Calculator {
     public int substract(int a, int b) {
         return a - b;
     }
+
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public float division(float a, float b) {
+        return a / b;
+    }
+
 }
